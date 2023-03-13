7b69cd7b749dd31d6310f5571ce31433ebd6b500
Rebeca <beka_victoria@hotmail.com>
2023-03-13 02:32:49 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+//switcher para trocar o tema clicando no botão
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
